1b0736c27af69f92a61dcbe88ed0bad53d02ca43
Robert McShinsky <robert.mcshinsky@gmail.com>
2021-03-21 19:18:55 -0600

    Created the first question and also information prompts

--- index.js
+++ index.js
@@ -3,114 +3,148 @@ console.log("hello");
 //const fs = require("fs");
 const inquirer = require("inquirer");
 
-inquirer
-  .prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "What is your name?",
-    },
-  ])
-  .then((answers) => console.log(answers));
-
-//
-
-//
-
-//
-
-//
-
-//
-
 // MAIN FUNCTION
 // ASKS THE QUESTIONS AT THE BEGINNING
-function main() {}
+function main() {
+  return inquirer.prompt([
+    {
+      type: "checkbox",
+      name: "createReadme",
+      message: "Check all that apply for your custom readme.",
+      choices: [
+        "Title",
+        "Description",
+        "Table of Contents",
+        "Installation",
+        "Instructions",
+        "Credits",
+        "License",
+        "Badges",
+        "Features",
+        "Contributers",
+        "Tests",
+        "Questions",
+      ],
+    },
+  ]);
+}
 
 // ASKS FOR A TITLE
-function title() {}
-
-// ASKS FOR A DESCRIPTION
-function description() {
-  let title = "Description";
-  information(title);
-}
-
-// CREATES TABLE OF CONTENTS
-function tableOfContents() {}
-
-// ASKS FOR INSTALLATION STEPS
-function installation() {
-  let title = "Installation";
-  information(title);
-}
-
-// ASKS FOR THE USAGE AND INSTRUCTIONS
-function instructions() {
-  let title = "Instructions";
-  information(title);
-}
-
-// ASKS FOR CREDITS AND ANY COLLABORATORS
-function credits() {
-  let title = "Credits";
-  information(title);
-}
-
-// ASKS FOR LICENSE
-function license() {
-  let title = "License";
-  information(title);
-}
-
-// ASKS FOR BADGES
-function badges() {
-  let title = "Badges";
-  information(title);
-}
-
-// ASKS FOR THE FEATURES OF THE APP
-function features() {
-  let title = "Features";
-  information(title);
-}
-
-// ASKS IF YOU WOULD LIKE OTHERS TO CONTRIBUTE TO THE APP
-function contributers() {
-  let title = "Contributers";
-  information(title);
-}
-
-// MAYBE ASK FOR TESTS
-function tests() {
-  let title = "Tests";
-  information(title);
-}
-
-// ASKS FOR QUESTIONS -- KINDA LIKE A CONTACT ME SECTION
-function questions() {
-  let title = "Questions";
-  information(title);
-  // ASKS FOR GITHUB USERNAME
-
-  // ASKS FOR EMAIL
+function title() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is the title of this project?",
+    },
+  ]);
 }
 
 // ASKS FOR THE INFORMATION UNDERNEATH THE SUB TITLE
 function information(title) {
-  let question1 = prompt("Is this title: (" + title + ") correct?");
-
+  if (title === "Title") {
+    return inquirer.prompt([
+      {
+        type: "input",
+        name: "title",
+        message: "What is the Title for your project?",
+      },
+    ]);
+  }
   if (title === "Description") {
-  } else if (title === "Installation") {
-  } else if (title === "Instructions") {
-  } else if (title === "Credits") {
-  } else if (title === "License") {
-  } else if (title === "Badges") {
-  } else if (title === "Features") {
-  } else if (title === "Contributers") {
-  } else if (title === "Tests") {
-  } else if (title === "Questions") {
-  } else {
-    return 0;
+    return inquirer.prompt([
+      {
+        type: "input",
+        name: "Description",
+        message: "How would you describe this project?",
+      },
+    ]);
+  }
+  if (title === "Installation") {
+    return inquirer.prompt([
+      {
+        type: "input",
+        name: "Installation",
+        message: "How would you install this project?",
+      },
+    ]);
+  }
+  if (title === "Instructions") {
+    return inquirer.prompt([
+      {
+        type: "input",
+        name: "Instructions",
+        message: "How can you use this project?",
+      },
+    ]);
+  }
+  if (title === "Credits") {
+    return inquirer.prompt([
+      {
+        type: "input",
+        name: "Credits",
+        message: "Who was a part of the creation of this project?",
+      },
+    ]);
+  }
+  if (title === "License") {
+    return inquirer.prompt([
+      {
+        type: "checkbox",
+        name: "License",
+        message: "Which licences do you want to include in this project?",
+        choices: ["Licence1", "Licence2", "Etc."],
+      },
+    ]);
+  }
+  if (title === "Badges") {
+    return inquirer.prompt([
+      {
+        type: "checkbox",
+        name: "Badges",
+        message: "Which badges do you want to include in this project?",
+        choices: ["Choice1", "Choice2", "Etc."],
+      },
+    ]);
+  }
+  if (title === "Features") {
+    return inquirer.prompt([
+      {
+        type: "input",
+        name: "Features",
+        message: "What are a few features in this project?",
+      },
+    ]);
+  }
+  if (title === "Contributers") {
+    return inquirer.prompt([
+      {
+        type: "input",
+        name: "Contributers",
+        message: "Who are some contributers in this project?",
+      },
+    ]);
+  }
+  if (title === "Tests") {
+    return inquirer.prompt([
+      {
+        type: "input",
+        name: "Tests",
+        message: "What kind of tests are recomended for this project?",
+      },
+    ]);
+  }
+  if (title === "Questions") {
+    return inquirer.prompt([
+      {
+        type: "confirm",
+        name: "Questions",
+        message:
+          "Do you want to include a contact the developer section for this project?",
+        default: false,
+      },
+    ]);
   }
 }
+
+main().then((answer) => console.log(answer));
