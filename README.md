aff4b58d1f8e5c6159fda3490f477e864f88a5b6
Robert McShinsky <robert.mcshinsky@gmail.com>
2021-03-28 21:17:53 -0600

    fixing license object

--- README.md
+++ README.md
@@ -1,30 +1,16 @@
 
-# My project 3
-## Description
-There are many ways to enjoy this application
+# asdf
 ## Table of Contents
-
-[Installation](#installation)
-
-[Instructions](#instructions)
 
 [License](#license)
 
 [Badges](#badges)
 
-[Contributers](#contributers)
-
 [Questions](#questions)
 
-## Installation
-through git install stuff
-## Instructions
-to create a readme much easier
 ## License
-GNU AGPLv3,GNU GPLv3,GNU LGPLv3,Mozilla Public License 2.0,Apache License 2.0,MIT License,Boost Software License 1.0,The Unlicense
+
 ## Badges
-Choice1,Choice2
-## Contributers
- 
+
 ## Questions
-true
+false
--- index.js
+++ index.js
@@ -17,61 +17,76 @@ let licenseNames = [
   "WTFPL",
   "Zlib",
 ];
+
 let licenses = [
   {
-    Apache:
+    name: "Apache",
+    link:
       "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
   },
   {
-    Boost:
+    name: "Boost",
+    link:
       "[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)",
   },
   {
-    BSD:
+    name: "BSD",
+    link:
       "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)",
   },
   {
-    Eclipse:
+    name: "Eclipse",
+    link:
       "[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)",
   },
   {
-    GNU:
+    name: "GNU",
+    link:
       "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
   },
   {
-    IBM:
+    name: "IBM",
+    link:
       "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)",
   },
   {
-    ISC:
+    name: "ISC",
+    link:
       "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)",
   },
   {
-    MIT:
+    name: "MIT",
+    link:
       "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
   },
   {
-    Mozilla:
+    name: "Mozilla",
+    link:
       "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)",
   },
   {
-    Perl:
+    name: "Perl",
+    link:
       "[![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)",
   },
   {
-    SIL:
+    name: "SIL",
+    link:
       "[![License: Open Font-1.1](https://img.shields.io/badge/License-OFL%201.1-lightgreen.svg)](https://opensource.org/licenses/OFL-1.1)",
   },
   {
-    Unlicense:
+    name: "Unlicense",
+    link:
       "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)",
   },
   {
-    WTFPL:
+    name: "WTFPL",
+    link:
       "[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)",
   },
   {
-    Zlib:
+    name: "Zlib",
+    link:
       "[![License: Zlib](https://img.shields.io/badge/License-Zlib-lightgrey.svg)](https://opensource.org/licenses/Zlib)",
   },
 ];
@@ -246,11 +261,11 @@ function generateReadme(response, user) {
     console.log("Length: " + response.license.length);
     console.log("TRUE OR NOT: " + licenseNames.includes(response.license[0]));
     console.log("before");
-    console.log("LICENSE RESPONSE: " + licenses[response.license][0]);
+    console.log("LICENSE RESPONSE: " + response.license[0]);
     console.log("after");
     for (let i = 0; i < response.license.length; ++i) {
       if (licenseNames.includes(response.license[i])) {
-        license += licenses.response.license[i];
+        console.log("SUPER TRUE");
       }
     }
     console.log("DONE");
